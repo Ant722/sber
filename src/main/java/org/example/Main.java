873bdce75effcaed8b264a70f2967cef3826c6ab
Ant722 <Ant722@mail.ru>
2023-07-28 00:22:07 +0300

    add files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,52 @@
 package org.example;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static List<City> cities = new ArrayList<>();
+    static File fileCities = new File("cityFile.csv");
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        loadCitiesFromFile();
+
+        for (City city : cities) {
+            System.out.println(city);
+            System.out.println();
+        }
+
+
     }
+
+    private static void loadCitiesFromFile() throws FileNotFoundException {
+        Scanner scanner = new Scanner(fileCities);
+
+        while (scanner.hasNext()) {
+
+            String[] str = scanner.nextLine().split(";");
+            CityBuilder cityBuilder = new CityBuilder()
+                    .name(str[1])
+                    .region(str[2])
+                    .district(str[3])
+                    .population(parseInt(str[4]));
+            if (str.length > 5) {
+                City city = cityBuilder
+                        .foundation(str[5])
+                        .build();
+                cities.add(city);
+            } else {
+                City city = cityBuilder.build();
+                cities.add(city);
+            }
+
+        }
+    }
+
 }
